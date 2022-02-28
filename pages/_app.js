b7aab2c3ec5a20fcc8c4218bcd42fa04b5ef5eab
mackenzieoster <mackenzieoster18@gmail.com>
2022-02-27 20:44:18 -0500

    2/27

--- pages/_app.js
+++ pages/_app.js
@@ -48,6 +48,36 @@ function userLoggedInFetch(app) {
   };
 }
 
+function AccountConnectionExample() {
+  const [connected, setConnected] = useState(false);
+  const accountName = connected ? "CHELA" : "";
+  const handleAction = useCallback(() => {
+    setConnected((connected) => !connected);
+  }, [connected]);
+  const buttonText = connected ? "Disconnect" : "Connect";
+  const details = connected ? "Account connected" : "No account connected";
+  const terms = connected ? null : (
+    <p>
+      By clicking <strong>Connect</strong>, you agree to accept Sample App’s{" "}
+      <Link url="CHELA account">terms and conditions</Link>. You’ll pay a
+      commission rate of 15% on sales made through Sample App.
+    </p>
+  );
+  return (
+    <AccountConnection
+      title="CHELA account"
+      action={{
+        content: buttonText,
+        onAction: handleAction,
+      }}
+      details={details}
+      termsOfService={terms}
+      connected={true}
+      accountName="CHELA"
+    />
+  );
+}
+
 function MyProvider(props) {
   const app = useAppBridge();
 
